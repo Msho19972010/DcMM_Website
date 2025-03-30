b939e8f19eb099206f779f81895e7ead70233b1d
Misha <98160546+Msho19972010@users.noreply.github.com>
2025-03-30 23:24:36 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// function revealOnScroll() {
+//   const sections = document.querySelectorAll(".section");
+//   sections.forEach((section) => {
+//     const sectionTop = section.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight;
+//     const revealPoint = 150; // Момент появления элемента
+
+//     if (sectionTop < windowHeight - revealPoint) {
+//       section.classList.add("revealed");
+//     }
+//   });
+// }
+
+// window.addEventListener("scroll", revealOnScroll);
+
+// // Вызываем функцию один раз при загрузке страницы
+// revealOnScroll();
